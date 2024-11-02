7b205249963af91fc56b266981eaa6663e31f1ff
Z1xus <40185941+Z1xus@users.noreply.github.com>
2024-11-02 07:47:55 +0100

    update deps, make site more accessible, fix error message, format

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-vercel";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	onwarn: (warning, handler) => {
-		if (warning.code.startsWith('a11y-')) {
+		if (warning.code.startsWith("a11y-")) {
 			return;
 		}
 		handler(warning);
